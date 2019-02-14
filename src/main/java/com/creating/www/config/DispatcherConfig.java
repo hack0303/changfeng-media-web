8fd0463223d6b72b447cdacb050ee55a07aa86ff
hack0303 <1097037585@qq.com>
2019-02-14 10:58:36 +0800

    框架搭建

---
+++ src/main/java/com/creating/www/config/DispatcherConfig.java
@@ -0,0 +1,10 @@
+package com.creating.www.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.creating.www")
+public class DispatcherConfig {
+
+}
---
+++ src/main/java/com/creating/www/core/EnterPoint.java
@@ -0,0 +1,28 @@
+package com.creating.www.core;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.creating.www.config.DispatcherConfig;
+import com.creating.www.config.RootConfig;
+
+public class EnterPoint implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigApplicationContext rootCtx=new AnnotationConfigApplicationContext();
+		rootCtx.register(RootConfig.class);
+		AnnotationConfigWebApplicationContext dispatcherCtx=new AnnotationConfigWebApplicationContext();
+		dispatcherCtx.register(DispatcherConfig.class);
+		ServletRegistration.Dynamic dispatcher=servletContext.addServlet("dispatcher",new DispatcherServlet(dispatcherCtx));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/*");
+	}
+
+}
